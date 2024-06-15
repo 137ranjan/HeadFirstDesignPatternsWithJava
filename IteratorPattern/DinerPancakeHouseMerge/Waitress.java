ca4d875c7d03ce7860ecc0394830a976672717cd
Ranjan Kumar Singh <137ranjan@gmail.com>
2024-06-15 17:37:45 +0530

    Implement iterator using hashmap

--- IteratorPattern/DinerPancakeHouseMerge/Waitress.java
+++ IteratorPattern/DinerPancakeHouseMerge/Waitress.java
@@ -5,20 +5,25 @@ import java.util.Iterator;
 public class Waitress {
     Menu pancakeHouseMenu;
     Menu dinerMenu;
+    Menu cafMenu;
 
-    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu) {
+    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
         this.pancakeHouseMenu = pancakeHouseMenu;
         this.dinerMenu = dinerMenu;
+        this.cafMenu = cafeMenu;
     }
 
     public void printMenu() {
         Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
         Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
+        Iterator<MenuItem> cafeIterator = cafMenu.createIterator();
 
         System.out.println("MENU\n------\nBREAKFAST");
         printMenu(pancakeIterator);
         System.out.println("\nLunch");
         printMenu(dinerIterator);
+        System.out.println("\nDINNER");
+        printMenu(cafeIterator);
     }
 
     private void printMenu(Iterator<MenuItem> iterator) {
